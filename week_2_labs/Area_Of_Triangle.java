2d226719f35e25f0d0c0277c405ca36afb324ccf
khanzaid94 <56857119+khanzaid94@users.noreply.github.com>
2021-07-11 15:00:24 +0530

    Add files via upload

---
+++ week_2_labs/Area_Of_Triangle.java
@@ -0,0 +1,18 @@
+package week_2_labs;
+
+import java.util.Scanner;
+
+public class Area_Of_Triangle {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the base of the Triangle:");
+		double base = input.nextDouble();
+		System.out.println("Enter the height of the Triangle:");
+		double height = input.nextDouble();
+		//Area = (width*height)/2
+		double area = (base*height)/2;
+		System.out.println("Area of Triangle is: " + area);
+		}
+		}
