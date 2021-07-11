c22817c71d9634f9df4cbe0defe2172db90f9434
khanzaid94 <56857119+khanzaid94@users.noreply.github.com>
2021-07-11 14:24:55 +0530

    Add files via upload

---
+++ week_2_labs/Switch_Case.java
@@ -0,0 +1,22 @@
+package week_2_labs;
+
+public class Switch_Case {
+
+	public static void main(String[] args) {
+		int tech = 1;
+		switch(tech){
+		case 1:
+		{
+		System.out.println("java");
+		break;
+		}
+		case 2:{
+		System.out.println("ES6");
+		break;
+		}
+		default:{
+		System.out.println("Not Listed..");
+		}
+		}
+	}
+}
