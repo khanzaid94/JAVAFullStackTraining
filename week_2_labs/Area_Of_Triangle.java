4ea64605eb96a8ca67f282aaf35eb21e06dd5a21
khanzaid94 <56857119+khanzaid94@users.noreply.github.com>
2021-07-11 15:01:13 +0530

    Update Area_Of_Triangle.java

--- week_2_labs/Area_Of_Triangle.java
+++ week_2_labs/Area_Of_Triangle.java
@@ -11,7 +11,7 @@ public class Area_Of_Triangle {
 		double base = input.nextDouble();
 		System.out.println("Enter the height of the Triangle:");
 		double height = input.nextDouble();
-		//Area = (width*height)/2
+		//Area of Triangle= (width*height)/2
 		double area = (base*height)/2;
 		System.out.println("Area of Triangle is: " + area);
 		}
