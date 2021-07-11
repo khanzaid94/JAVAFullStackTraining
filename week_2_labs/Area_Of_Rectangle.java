95553c611b9c3c3d468a7b984e2cdf0ce8a81fe1
khanzaid94 <56857119+khanzaid94@users.noreply.github.com>
2021-07-11 14:39:48 +0530

    Add files via upload

---
+++ week_2_labs/Area_Of_Rectangle.java
@@ -0,0 +1,18 @@
+package week_2_labs;
+
+import java.util.Scanner;
+
+public class Area_Of_Rectangle {
+
+	public static void main(String[] args) {
+		
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter the length of Rectangle:");
+			double length = input.nextDouble();
+			System.out.println("Enter the width of Rectangle:");
+			double width = input.nextDouble();
+			//Area = length*width;
+			double area = length*width;
+			System.out.println("Area of Rectangle is:"+area);
+			}
+			}
